818389f829ce5378962b3d5a25966fd16b25bab6
misaki80074 <misaki80074@gmail.com>
2024-12-05 16:29:59 +0800

    20241205 修改 Login 頁面

--- obj/Debug/net8.0/Coffee.AssemblyInfo.cs
+++ obj/Debug/net8.0/Coffee.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Coffee")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16211f7c8451fe5b9c1604fcf81963704ab669a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+427a7b557192f849b7e79a2fa551ece3384d038f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Coffee")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Coffee")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
