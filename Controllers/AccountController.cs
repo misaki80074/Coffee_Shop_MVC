397e1193c3eb09a076542c1cc264327f58f34f12
pisces85317 <pisces85317@gmail.com>
2024-12-05 21:24:10 +0800

    1205:提取分支、加入動作篩選、注入資料庫

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,9 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Coffee.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace Coffee.Controllers
 {
     public class AccountController : Controller
     {
+        private ProjectContext _context;
+
+        public AccountController(ProjectContext dbContext)
+        {
+            _context = dbContext;
+        }
+
         public IActionResult Login()
         {
             return View();
@@ -14,9 +24,23 @@ namespace Coffee.Controllers
             return View();
         }
 
+        [AuthFilter]
         public IActionResult Member()
         {
             return View();
+        }
+    }
+    public class AuthFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            // 檢查是否有登入
+            if (filterContext.HttpContext.Session.GetString("userid") == null)
+            {
+                // 未登入，重新定向至登入頁面
+                filterContext.Result = new RedirectResult("Login");
+            }
+            base.OnActionExecuting(filterContext);
         }
     }
 }
