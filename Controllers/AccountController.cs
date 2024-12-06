50603e409447ba8dcca72e53ee1121d68d978ee5
pisces85317 <pisces85317@gmail.com>
2024-12-06 16:38:07 +0800

    1206:查詢訂單明細待修改

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -2,6 +2,7 @@
 using Coffee.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Member.Controllers;
 
 namespace Coffee.Controllers
 {
@@ -28,19 +29,6 @@ namespace Coffee.Controllers
         public IActionResult Member()
         {
             return View();
-        }
-    }
-    public class AuthFilter : ActionFilterAttribute
-    {
-        public override void OnActionExecuting(ActionExecutingContext filterContext)
-        {
-            // 檢查是否有登入
-            if (filterContext.HttpContext.Session.GetString("userid") == null)
-            {
-                // 未登入，重新定向至登入頁面
-                filterContext.Result = new RedirectResult("Login");
-            }
-            base.OnActionExecuting(filterContext);
         }
     }
 }
